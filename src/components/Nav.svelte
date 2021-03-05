6ad1abf4de2568292c49ed0252584347a4203389
Hein Dauven <heindauven@gmail.com>
2021-03-05 09:13:08 +0100

    Add mobile and desktop navigation.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,46 @@
 <script>
+  let isOpen = false;
   export let segment;
+  function toggleNav() {
+    isOpen = !isOpen;
+  }
 </script>
+
+<style>
+  @media (min-width: 760px) {
+    nav {
+      border-bottom: solid gray 1px;
+    }
+  }
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a href="/" class="navbar-item">
+      <img src="logo.jpg" alt="Virus logo" />
+    </a>
+    <span
+      class:is-active={isOpen}
+      on:click={toggleNav}
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false">
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </span>
+  </div>
+  <div class="navbar-menu" class:is-active={isOpen}>
+    <div class="navbar-start">
+      <a href="/" class="navbar-item" class:is-active={segment === undefined}>
+        Home
+      </a>
+      <a
+        href="/about"
+        class="navbar-item"
+        class:is-active={segment === 'about'}>
+        About
+      </a>
+    </div>
+  </div>
+</nav>
